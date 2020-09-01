86778836f69eb3ef600abd6f76e162a7563ab4b3
pnkhero <46791333+pnkhero@users.noreply.github.com>
2020-09-01 14:43:48 +0200

    Add files via upload

--- crepes_bretonnes/blog/admin.py
+++ crepes_bretonnes/blog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.text import Truncator
 
-# Register your models here.
+from .models import Categorie, Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display    = ('titre', 'auteur', 'date', 'apercu_contenu')
+    list_filter     = ('auteur', 'categorie',)
+    date_hierarchy  = 'date'
+    ordering        = ('date', )
+    search_fields   = ('titre', 'contenu')
+
+    fieldsets = (
+        # Fieldset 1 : meta-info (titre, auteur…)
+       ('Général', {
+            'classes': ['collapse', ],
+            'fields': ('titre', 'auteur', 'categorie')
+        }),
+        # Fieldset 2 : contenu de l'article
+        ('Contenu de l\'article', {
+           'description': 'Le formulaire accepte les balises HTML. Utilisez-les à bon escient !',
+           'fields': ('contenu', )
+        }),
+    )
+
+    def apercu_contenu(self, article):
+        """
+        retourne les 40 premiers cara du contenu de l'article,
+        suivi de points de suspension si le text est plus long.
+        """
+        return Truncator(article.contenu).chars(40, truncate='...')
+
+    apercu_contenu.short_description = 'Apercu du contenu'    
+
+admin.site.register(Categorie)
+admin.site.register(Article, ArticleAdmin)
